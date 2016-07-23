d9e4181b26a1962d2272d92c3f6dcd1c790fbded
Athenas Sullivan <AthenasSullivan@athenass-macbook-pro.local>
2016-07-23 17:54:25 -0400

    Initial Commit

---
+++ CashDash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CashDash
+//
+//  Created by Athenas Sullivan on 7/23/16.
+//  Copyright © 2016 AirplaneMode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CashDashTests/CashDashTests.m
@@ -0,0 +1,39 @@
+//
+//  CashDashTests.m
+//  CashDashTests
+//
+//  Created by Athenas Sullivan on 7/23/16.
+//  Copyright © 2016 AirplaneMode. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CashDashTests : XCTestCase
+
+@end
+
+@implementation CashDashTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
